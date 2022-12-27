99558bc22790a905bd37765f1d19d1dc789dfce2
Dmitri Tokmashov <105676150+dmitri-tokmashov@users.noreply.github.com>
2022-12-28 02:56:55 +0300

    Objects Test
    • Static objects added
    • Destructible objects added
    • Collectible bonuses added

---
+++ scripts/destructible.gd
@@ -0,0 +1,20 @@
+extends Area;
+
+func init():
+	connect("body_entered", self, "body_entered");
+	monitoring = true;
+	monitorable = true;
+	get_node("collision").disabled = false;
+	visible = true;
+
+func body_entered(_body):
+	if _body is RigidBody and _body.mode == RigidBody.MODE_RIGID:
+		disconnect("body_entered", self, "body_entered");
+		for _part in [get_node("head"), get_node("torso"), get_node("legs")]:
+			var _transform = _part.global_transform;
+			_part.mode = RigidBody.MODE_RIGID;
+			_part.set_collision_mask_bit(0, true);
+			get_parent().destructibles.append(_part);
+			remove_child(_part);
+			get_parent().add_child(_part);
+			_part.transform = _transform;
