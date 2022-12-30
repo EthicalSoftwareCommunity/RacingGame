b42889d521783433fe089ee7325449185f488e6e
Dmitri Tokmashov <105676150+dmitri-tokmashov@users.noreply.github.com>
2022-12-30 04:37:58 +0300

    Road Test
    • Road generation implementation

---
+++ scripts/road.gd
@@ -0,0 +1,52 @@
+extends MeshInstance;
+
+var body = StaticBody.new();
+var collision = CollisionShape.new();
+#Vector2 type variable is used for the calculation.
+#x — latitude curve modifier, must be a multiple of 2
+#y — longitude curve modifier, must be between 0 and 1
+export (Vector2) var modifier = Vector2.ONE;# Value will be saved and visible in the property editor.
+export (Vector2) var size = Vector2(1024, 8);
+export (float) var height = 0.325;
+export (float) var width = 0.01;
+onready var _torus = get_parent().get_node("torus");
+
+func _ready():
+	add_child(body);
+	body.add_child(collision);
+	body.set_collision_layer_bit(31, true);
+	var _data = [];
+	var _positions = [];
+	var _vectors = PoolVector3Array();
+	_data.resize(ArrayMesh.ARRAY_MAX);
+	for _x in range(size.x):
+		for _y in range(size.y):
+			_vectors += _road_segment(Vector2(_x, _y));
+	_data[ArrayMesh.ARRAY_VERTEX] = _vectors;
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, _data);
+	collision.shape = mesh.create_trimesh_shape();
+
+func _road_segment(vector):
+	var _positions = [position(vector.x), position(vector.x + 1), position(vector.x + 2)];
+	var _angles = [
+		(_positions[1] - _positions[0]).normalized().rotated(-PI / 2).angle(),
+		(_positions[2] - _positions[1]).normalized().rotated(-PI / 2).angle()
+	];
+	var _vectors = PoolVector3Array();
+	var _vectors_positions = [
+		_positions[0] + Vector2(-width / 2 + vector.y / size.y * width, 0).rotated(_angles[0]),
+		_positions[0] + Vector2(-width / 2 + (vector.y + 1) / size.y * width, 0).rotated(_angles[0]),
+		_positions[1] + Vector2(-width / 2 + vector.y / size.y * width, 0).rotated(_angles[1]),
+		_positions[1] + Vector2(-width / 2 + (vector.y + 1) / size.y * width, 0).rotated(_angles[1])
+	];
+	_vectors.append(_torus.position(_vectors_positions[0]) + _torus.normal(_vectors_positions[0]) * height);
+	_vectors.append(_torus.position(_vectors_positions[1]) + _torus.normal(_vectors_positions[1]) * height);
+	_vectors.append(_torus.position(_vectors_positions[2]) + _torus.normal(_vectors_positions[2]) * height);
+	_vectors.append(_vectors[-1]);
+	_vectors.append(_vectors[-3]);
+	_vectors.append(_torus.position(_vectors_positions[3]) + _torus.normal(_vectors_positions[3]) * height);
+	return _vectors;
+
+func position(x):
+	var _angle = x / size.x * PI * 2 * modifier.x;
+	return Vector2(0.5 + sin(_angle) / 2 * modifier.y, x / size.x);
--- scripts/world.gd
+++ scripts/world.gd
@@ -5,38 +5,42 @@ var distance_maximum: float;
 var bonuses = [];
 var destructibles = [];
 var objects = [];
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * -100;
 export (float) var acceleration = 8192 * 2;
-export (float) var gravity = 9.8;
+export (Array) var amount = [100, 100, 100];
+export (Array) var distance = [0.04, 0.08, 0.06];
+export (Array) var width = [0.06, 0.12, 0.06];
 
 func _ready():
-	gravity *= -100;
+	randomize();
 	distance_maximum = pow($torus.height, 2);
 	$ball.translation = Vector3(0, -$torus.radius + 1, 0);
-	for _angle in range(90):
-		var _bonus = $bonus.duplicate();
-		add_child(_bonus);
-		bonuses.append(_bonus);
-		_bonus.transform = $torus.local_transform(_bonus, Vector2(0.495 + (_angle % 2) * 0.01, _angle / 90.0));
-		_bonus.translation = $torus.position(Vector2(0.495 + (_angle % 2) * 0.01, _angle / 90.0)) + $torus.normal(Vector2(0.495 + (_angle % 2) * 0.01, _angle / 90.0)) * 1;
-		_bonus.init();
-		if _angle % 2 == 0:
-			var _object = $static.duplicate();
-			add_child(_object);
-			objects.append(_object);
-			_object.transform = $torus.local_transform(_object, Vector2(0.475, (_angle + 0.25) / 45.0));
-			_object.translation = $torus.position(Vector2(0.475, (_angle + 0.25) / 45.0));
-			_object.init();
-			_object = $static.duplicate();
-			add_child(_object);
-			objects.append(_object);
-			_object.transform = $torus.local_transform(_object, Vector2(0.525, (_angle + 0.25) / 45.0));
-			_object.translation = $torus.position(Vector2(0.525, (_angle + 0.25) / 45.0));
-			_object.init();
-			_object = $destructible.duplicate();
-			add_child(_object);
-			_object.transform = $torus.local_transform(_object, Vector2(0.5, (_angle + 0.25) / 45.0));
-			_object.translation = $torus.position(Vector2(0.5, (_angle + 0.25) / 45.0));
-			_object.init();
+#	for _angle in range(90):
+#		var _bonus = $bonus.duplicate();
+#		add_child(_bonus);
+#		bonuses.append(_bonus);
+#		_bonus.transform = $torus.local_transform(_bonus, Vector2(0.495 + (_angle % 2) * 0.01, _angle / 90.0));
+#		_bonus.translation = $torus.position(Vector2(0.495 + (_angle % 2) * 0.01, _angle / 90.0)) + $torus.normal(Vector2(0.495 + (_angle % 2) * 0.01, _angle / 90.0)) * 1;
+#		_bonus.init();
+#		if _angle % 2 == 0:
+#			var _object = $static.duplicate();
+#			add_child(_object);
+#			objects.append(_object);
+#			_object.transform = $torus.local_transform(_object, Vector2(0.475, (_angle + 0.25) / 45.0));
+#			_object.translation = $torus.position(Vector2(0.475, (_angle + 0.25) / 45.0));
+#			_object.init();
+#			_object = $static.duplicate();
+#			add_child(_object);
+#			objects.append(_object);
+#			_object.transform = $torus.local_transform(_object, Vector2(0.525, (_angle + 0.25) / 45.0));
+#			_object.translation = $torus.position(Vector2(0.525, (_angle + 0.25) / 45.0));
+#			_object.init();
+#			_object = $destructible.duplicate();
+#			add_child(_object);
+#			_object.transform = $torus.local_transform(_object, Vector2(0.5, (_angle + 0.25) / 45.0));
+#			_object.translation = $torus.position(Vector2(0.5, (_angle + 0.25) / 45.0));
+#			_object.init();
+	_generation();
 
 func _physics_process(delta):
 	$ball.engine_force = direction.y * acceleration * delta;
@@ -46,6 +50,45 @@ func _physics_process(delta):
 	$camera.translation -= ($camera.translation - ($ball.to_global(Vector3(0, 1, 2)))) * delta * 16;
 	$camera.rotation -= _get_camera_rotation($camera, $ball) * delta * 4;
 	$camera.fov = 70 + $ball.linear_velocity.length() * 2;
+
+func _get_camera_rotation(_camera, _target) -> Vector3:
+	var _camera_rotation = _camera.rotation - _target.rotation;
+	for _axis in range(3):
+		if abs(_camera_rotation[_axis]) >= PI:
+			_camera_rotation[_axis] = -sign(_camera_rotation[_axis]) * fmod((2 * PI - abs(_camera_rotation[_axis])), PI);
+	return _camera_rotation;
+
+func _generation():
+	var _x: float;
+	var _position: Vector2;
+	var _angle: float;
+	for _counter in amount[0]:
+		_x = rand_range(0, $road.size.x);
+		_position = $road.position(_x) + Vector2((distance[0] + rand_range(0, 1000 * width[0]) / 1000) * sign(rand_range(-1, 1)), 0);
+		var _bonus = $bonus.duplicate();
+		add_child(_bonus);
+		bonuses.append(_bonus);
+		_bonus.transform = $torus.local_transform(_bonus, _position);
+		_bonus.translation = $torus.position(_position) + $torus.normal(_position) * 1;
+		_bonus.init();
+	for _counter in amount[1]:
+		_x = rand_range(0, $road.size.x);
+		_position = $road.position(_x) + Vector2((distance[1] + rand_range(0, 1000 * width[1]) / 1000) * sign(rand_range(-1, 1)), 0);
+		var _object = $static.duplicate();
+		add_child(_object);
+		objects.append(_object);
+		_object.transform = $torus.local_transform(_object, _position);
+		_object.translation = $torus.position(_position);
+		_object.init();
+	for _counter in amount[2]:
+		_x = rand_range(0, $road.size.x);
+		_position = $road.position(_x) + Vector2((distance[2] + rand_range(0, 1000 * width[2]) / 1000) * sign(rand_range(-1, 1)), 0);
+		var _object = $destructible.duplicate();
+		add_child(_object);
+		objects.append(_object);
+		_object.transform = $torus.local_transform(_object, _position);
+		_object.translation = $torus.position(_position);
+		_object.init();
 
 func gravity(object):
 	var _ray = get_node("ray");
@@ -62,10 +105,3 @@ func gravity(object):
 	_ray.get_parent().remove_child(_ray);
 	add_child(_ray);
 	return object.mass * gravity * _gravity;
-
-func _get_camera_rotation(_camera, _target) -> Vector3:
-	var _camera_rotation = _camera.rotation - _target.rotation;
-	for _axis in range(3):
-		if abs(_camera_rotation[_axis]) >= PI:
-			_camera_rotation[_axis] = -sign(_camera_rotation[_axis]) * fmod((2 * PI - abs(_camera_rotation[_axis])), PI);
-	return _camera_rotation;
